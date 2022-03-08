5a02b14d4903361f9274c0194975223339793eb5
thuanvan10121998 <95727165+thuanvan10121998@users.noreply.github.com>
2022-03-08 14:28:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bae';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Gọi Ngọc là google.Bởi Ngọc là tất cả những gì mà Thuấn tìm kiếm !';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'gg.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
